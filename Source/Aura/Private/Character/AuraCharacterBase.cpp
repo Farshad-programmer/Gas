eb62aff6943d2c29e5a22a9348f8c50bdd4cd358
Farshad Shirkhani <63773984+Farshad-programmer@users.noreply.github.com>
2023-09-22 12:02:54 +0300

    important file added , weapon mesh added to AuraCharacterBaseClass and also mesh and weapons added on thiere blueprint version

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -2,10 +2,14 @@
 
 
 #include "Character/AuraCharacterBase.h"
+#include "Components/SkeletalMeshComponent.h"
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 }
 
